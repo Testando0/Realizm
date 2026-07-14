70479231e27a1658bec91a7250ecdb4a6409d869
Testando0 <joh769637@gmail.com>
2026-07-14 16:50:27 -0300

    Add Next.js configuration with custom Webpack rules

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: { unoptimized: true },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag)$/,
+      type: 'asset/source',
+    });
+    return config;
+  },
+};
+
+module.exports = nextConfig;
